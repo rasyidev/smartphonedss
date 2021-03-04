cea405ae7c12ec01cca009ac7bdf41ac22332037
rasyidev <habib.rasyid11@gmail.com>
2021-03-04 14:10:01 +0700

    Create simple login system

--- smartphonedss/urls.py
+++ smartphonedss/urls.py
@@ -5,9 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='landing_page'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.user_register, name='register'),
     path('register-preference/', views.register_preference, name="register_preference"),
     path('register-success/', views.register_success, name="register_success"),
+    path('logout/', views.user_logout, name='logout'),
     path('admin/', admin.site.urls),
 ]
--- smartphonedss/views.py
+++ smartphonedss/views.py
@@ -1,23 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 def index(request):
    return render(request, 'index.html')
 
-def login(request):
+def user_login(request):
    print(request.method)
+   if (request.method=='POST'):
+      password= request.POST['password']
+      username= request.POST['username']
+      user = authenticate(request, username=username, password=password)
+      if(user is not None):
+         login(request, user)
+         return redirect('dashboard')
+      else:
+         return redirect('login')
+
+
    context = {
       'page_title': "Halaman Login",
    }
    return render(request, 'login.html', context)
 
-def register(request):
+def user_register(request):
    context = {
       'page_title': "Halaman Register",
    }
    return render(request, 'register.html', context)
 
 def register_preference(request):
+   form_result = None
+   if request.method == "POST":
+      form_result = request.POST or None
+     
+      performance_vs_price = form_result['performance_vs_price']
+      performance_vs_camera = form_result['performance_vs_camera']
+      performance_vs_memory = form_result['performance_vs_memory']
+      performance_vs_battery = form_result['performance_vs_battery']
+      performance_vs_reputation = form_result['performance_vs_reputation']
+      price_vs_camera = form_result['price_vs_camera']
+      price_vs_memory = form_result['price_vs_memory']
+      price_vs_battery = form_result['price_vs_battery']
+      price_vs_reputation = form_result['price_vs_reputation']
+      camera_vs_memory = form_result['camera_vs_memory']
+      camera_vs_battery = form_result['camera_vs_battery']
+      camera_vs_reputation = form_result['camera_vs_reputation']
+      memory_vs_battery = form_result['memory_vs_battery']
+      memory_vs_reputation = form_result['memory_vs_reputation']
+      battery_vs_reputation = form_result['battery_vs_reputation']
+
+
+   range_value = list(range(-9,10))
+   range_value.remove(-1)
+   range_value.remove(0)
+
+   form_input_data = [
+      {'name': 'performance_vs_price', 'left':'Performa', 'right':'Harga'},
+      {'name': 'performance_vs_camera', 'left':'Performa', 'right':'Kamera'},
+      {'name': 'performance_vs_memory', 'left':'Performa', 'right':'Kapasitas Memori Internal'},
+      {'name': 'performance_vs_battery', 'left':'Performa', 'right':'Kapasitas Baterai'},
+      {'name': 'performance_vs_reputation', 'left':'Performa', 'right':'Reputasi Produk'},
+      {'name': 'price_vs_camera', 'left':"Harga", 'right': "Kamera"},
+      {'name': 'price_vs_memory', 'left':"Harga", 'right': "Kapasitas Memori Internal"},
+      {'name': 'price_vs_battery', 'left':"Harga", 'right': "Kapasitas Baterai"},
+      {'name': 'price_vs_reputation', 'left':"Harga", 'right': "Reputasi Produk"},
+      {'name': 'camera_vs_memory', 'left':'Kamera', 'right':'Kapasitas Memori Internal'},
+      {'name': 'camera_vs_battery', 'left':'Kamera', 'right':'Kapasitas Baterai'},
+      {'name': 'camera_vs_reputation', 'left':'Kamera', 'right':'Reputasi Produk'},
+      {'name': 'memory_vs_battery', 'left':'Kapasitas Memori Internal', 'right':'Kapasitas Baterai'},
+      {'name': 'memory_vs_reputation', 'left':'Kapasitas Memori Internal', 'right':'Reputasi Produk'},
+      {'name': 'battery_vs_reputation', 'left':'Kapasitas Memori Internal', 'right':'Reputasi Produk'},
+      {'name': 'ram_vs_cpu', 'left':'RAM', 'right':'CPU'},
+      {'name': 'rating_vs_sold', 'left':'Rating', 'right':'Jumlah Produk Terjual'}
+   ]
+
    context = {
+      'range_value': range_value,
+      'input': form_input_data,
+      'form_result' : form_result,
       'page_title': "Register Preferensi Pengguna"
    }
    return render(request, 'register-preference.html', context)
@@ -27,3 +88,14 @@ def register_success(request):
       'page_title': "Berhasil Register"
    }
    return render(request, 'register-success.html', context)
+
+
+def dashboard(request):
+   context = {
+      'page_title': "Dashboard"
+   }
+   return render(request, 'dashboard.html', context)
+
+def user_logout(request):
+   logout(request)
+   return redirect('/')
